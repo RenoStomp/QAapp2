3a249c51d246d17388a5a957f671ada897c2b329
Emil Abdullaev <renoemilll@gmail.com>
2023-02-24 21:34:31 +0600

    Repository and IRepository are finished

--- QAapp.Data/Repositories/Implementations/DbRepository.cs
+++ QAapp.Data/Repositories/Implementations/DbRepository.cs
@@ -1,6 +1,78 @@
-﻿namespace QAapp.Data.Repositories.Implementations
+﻿using QAapp.Data.Model.Common;
+using QAapp.Data.Repositories.Interfaces;
+using QAapp.Data.SqlServer;
+
+namespace QAapp.Data.Repositories.Implementations
 {
-    public class DbRepository
+    public class DbRepository<T> : IRepository<T>
+        where T : BaseEntity
     {
+        protected readonly AppDbContext Ctx;
+
+        public DbRepository(AppDbContext ctx)
+        {
+            Ctx = ctx;
+        }
+        public T Create(T entity)
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return null;
+
+            table.Add(entity);
+            Ctx.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return;
+
+            table.Remove(entity);
+            Ctx.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return;
+
+            var entity = ReadById(id);
+            table.Remove(entity);
+            Ctx.SaveChanges();
+        }
+
+        public bool IsExists(int id)
+        {
+            return Ctx.Set<T>().Any(entity => entity.ID.Equals(id));
+        }
+
+        public List<T> ReadAll()
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return null;
+            
+            var list = table.ToList();
+            return list;
+        }
+
+        public T ReadById(int id)
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return null;
+            
+            var entity = table.FirstOrDefault(en => en.ID.Equals(id));
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            var table = Ctx.Set<T>();
+            if (table == null) return null;
+            
+            table.Update(entity);
+            Ctx.SaveChanges();
+            return entity;
+        }
     }
 }
--- QAapp.Data/Repositories/Interfaces/IRepository.cs
+++ QAapp.Data/Repositories/Interfaces/IRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace QAapp.Data.Repositories.Interfaces
+﻿using QAapp.Data.Model.Common;
+
+namespace QAapp.Data.Repositories.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T>
+        where T : BaseEntity
     {
+
+        public T Create(T entity);   //C
+        public List<T> ReadAll();    //R
+        public T ReadById(int id);   //R
+        public T Update(T entity);   //U
+        public void Delete(T entity);   //D
+        public void DeleteById(int id); //D
+        public bool IsExists(int id);
+
     }
 }
