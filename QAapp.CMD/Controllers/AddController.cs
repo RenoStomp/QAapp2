ecda45d8dd7ca999fedda2bbf5facade2463832e
Emil Abdullaev <renoemilll@gmail.com>
2023-02-27 10:10:01 +0600

    AddController finished Todo: Client and Order readers

--- QAapp.CMD/Controllers/AddController.cs
+++ QAapp.CMD/Controllers/AddController.cs
@@ -1,5 +1,7 @@
-﻿using QAapp.CMD.Menu;
+﻿using QAapp.CMD.Controllers.AddHelpers;
+using QAapp.CMD.Menu;
 using QAapp.Data.Model.Common;
+using QAapp.Data.Model.Entities;
 
 namespace QAapp.CMD.Controllers
 {
@@ -13,17 +15,23 @@ namespace QAapp.CMD.Controllers
             switch (index)
             {
                 case 0:
-
+                    AddClient();
                     break;
                 case 1:
-
+                    AddOrder();
                     break;
             }
         }
 
         public static void AddClient()
         {
-
+            Client client = ClientReader.CreateClient();
+            MainMenu._clientController.Create(client);
+        }
+        public static void AddOrder()
+        {
+            Order order = OrderReader.CreateOrder();
+            MainMenu._orderController.Create(order);
         }
     }
 }
