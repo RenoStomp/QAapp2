b5597f91a7d7b0c99b2daf20e7ccfec6d6a74931
Emil Abdullaev <renoemilll@gmail.com>
2023-02-27 10:32:39 +0600

    Constructors to Order and Client added

--- QAapp.Data/Model/Entities/Client.cs
+++ QAapp.Data/Model/Entities/Client.cs
@@ -14,6 +14,15 @@ namespace QAapp.Data.Model.Entities
         public DateTime DateAdd { get; set; }
         public ICollection<Order> Orders { get; set; }
 
+        public Client(string phoneNum, DateTime dateAdd, string firstName, string secondName)
+        {
+            PhoneNum = phoneNum;
+            DateAdd = dateAdd;
+            FirstName = firstName;
+            SecondName = secondName;
+            DateAdd = DateTime.Now;
+        }
+
         public override string ToString()
         {
             return $"{ID} | {FirstName} | {SecondName} | {OrderAmount} | {DateAdd.Date} | {PhoneNum}";
--- QAapp.Data/Model/Entities/Order.cs
+++ QAapp.Data/Model/Entities/Order.cs
@@ -5,12 +5,26 @@ namespace QAapp.Data.Model.Entities
     public class Order : BaseEntity
     {
         public DateTime? OrderDate { get; set; }
-        public uint ClientID { get; set; }
+        public uint ClientID { 
+            get
+            {
+                return Client.ID;
+            }
+        }
         public string Description { get; set; }
         public float OrderPrice { get; set; }
         public DateTime CloseDate { get; set; }
         public Client Client { get; set; }
 
+        public Order(DateTime closeDate, string description, float orderPrice, Client client) 
+        {
+            OrderDate = DateTime.Now;
+            CloseDate = closeDate;
+            Description = description;
+            OrderPrice = orderPrice;
+            Client = client;
+
+        }
         public override string ToString()
         {
             return $"{ID} | {Description} | {Client.FullName} | {OrderPrice} | {CloseDate.Date}";
