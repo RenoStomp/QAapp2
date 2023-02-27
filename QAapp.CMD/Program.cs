78428b4b893d46b466efba5d4c25fdd2081691dc
Emil Abdullaev <renoemilll@gmail.com>
2023-02-27 19:22:13 +0600

    Last comment Last warrior

--- QAapp.CMD/Program.cs
+++ QAapp.CMD/Program.cs
@@ -3,5 +3,6 @@
 Console.WriteLine("Do not press anything\n" +
                   "Loading...");
 
-
+//там много моментов, где пара методов могут быть описаны одним genericом, но уже правда нет сил
+//прастити
 MainMenu.Execute();
